28e1f36afad93ae4b668267217fd890afaf96d80
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-06-29 18:52:18 -0400

    more playing around through course

--- frot-end-masters-vue-intro/fem-dirs-an-data-rendering/src/components/HelloWorld.vue
+++ frot-end-masters-vue-intro/fem-dirs-an-data-rendering/src/components/HelloWorld.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="hello">
+  <div class="hello" @click="entered = !entered" :class="entered && 'entered'">
+    <!-- : === v-bind: -->
     <ul>
-      <li v-for="num in 5" v-bind:key="num">{{ num }}</li>
+      <li
+        v-for="num in 5"
+        v-bind:class="checkedNames.length >= num ? 'is-checked' : ''"
+        v-bind:key="num"
+      >{{ num }}</li>
     </ul>
     <h3>{{ message }}</h3>
     <span class="input-box" v-bind:key="person" v-for="person in people">
       <label for="person">{{ person }}</label>
-      <input type="checkbox" :id="person" :value="person" v-model="checkedNames">
+      <!-- @ === v-on:... can user terinaries and such directly here, or methods which will auto pass "e" -->
+      <input
+        @click="clickingCheckBox"
+        type="checkbox"
+        :id="person"
+        :value="person"
+        v-model="checkedNames"
+      >
     </span>
+    <!-- difference between v-if and v-show is in the dom. v-if keeps a place holder comment for the item and dosnt render it.
+    v-show creates the element in the dom and puts a display none on it-->
     <h4 v-if="checkedNames.length === 5">everyone checked! :D</h4>
+    <div class="coolguy" v-if="checkedNames">
+      <p v-if="checkedNames.length < 5">ur mean!</p>
+      <p v-else>yeaaaa thats what i thought!</p>
+    </div>
     <p v-bind:key="name" v-for="name in checkedNames">{{name}}</p>
+    <!-- <pre v-pre>{{ message }}</pre> this will pretty print -->
+    <div class="calculator">
+      <input v-model.number="num1" type="number">
+      <label>+</label>
+      <input v-model.number="num2" type="number">
+      <label>=</label>
+      <label>{{ num1 + num2 }}</label>
+    </div>
   </div>
+  <!-- can do things like @submit.prevent to prevent default, or @click.once to only allow 1 click -->
+  <!-- keycodes are avaibale, for example @enter. can also add your own. -->
 </template>
 
 <script>
@@ -21,10 +49,18 @@ export default {
   },
   data() {
     return {
-      message: "hi there :D",
+      message: "favorite peeps?",
       people: ["Tom", "Moriah", "Sally", "Wendy", "Frank"],
-      checkedNames: []
+      checkedNames: [],
+      entered: false,
+      num1: 0,
+      num2: 0
     };
+  },
+  methods: {
+    clickingCheckBox(e) {
+      console.log(e.target.value);
+    }
   }
 };
 </script>
@@ -48,5 +84,15 @@ a {
 .input-box {
   padding: 8px;
   border-bottom: 1px solid black;
+}
+.is-checked {
+  color: rgb(196, 27, 27);
+}
+.coolguy {
+  color: rgb(0, 74, 184);
+}
+.entered {
+  background: rgba(0, 0, 0, 0.01);
+  padding-bottom: 16px;
 }
 </style>
