7c7fbfb8754791275ba1a28667463a2d80b48500
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2019-06-29 17:43:24 -0400

    starting work through front end masters vue course. directives are AWESOME. the flexibility of v-model is so cool.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Vue-Learning
+
+Learning Vue for my part in the "doorman" project for Lambda School's July 2019 Hackathon!
---
+++ frot-end-masters-vue-intro/fem-dirs-an-data-rendering/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="num in 5" v-bind:key="num">{{ num }}</li>
+    </ul>
+    <h3>{{ message }}</h3>
+    <span class="input-box" v-bind:key="person" v-for="person in people">
+      <label for="person">{{ person }}</label>
+      <input type="checkbox" :id="person" :value="person" v-model="checkedNames">
+    </span>
+    <h4 v-if="checkedNames.length === 5">everyone checked! :D</h4>
+    <p v-bind:key="name" v-for="name in checkedNames">{{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message: "hi there :D",
+      people: ["Tom", "Moriah", "Sally", "Wendy", "Frank"],
+      checkedNames: []
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.input-box {
+  padding: 8px;
+  border-bottom: 1px solid black;
+}
+</style>
